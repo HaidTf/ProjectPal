2ec09991c009870627ee26d1b0f1033a9ed58e38
haidtf <haidartfeily25@gmail.com>
2023-04-20 15:17:11 +0300

    implemented test methods for jwt filter

---
+++ src/test/java/com/projectpal/security/filter/JwtAuthenticationFilterTest.java
@@ -0,0 +1,69 @@
+package com.projectpal.security.filter;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.projectpal.entity.User;
+import com.projectpal.entity.enums.Role;
+import com.projectpal.service.JwtService;
+
+
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JwtAuthenticationFilterTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Autowired
+    private JwtService jwtService;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
+
+    @Test
+    public void testWithValidJwt() throws Exception {
+        
+    	User user = new User("haid", "haidar@gmail.com", "1234", Role.USER,null);
+       
+       String jwt = jwtService.generateToken(user);
+       
+       Mockito.when(userDetailsService.loadUserByUsername(user.getUsername())).thenReturn(user);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/test")
+                .header("Authorization", "Bearer " + jwt))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+    @Test
+    public void testWithInValidJwt() throws Exception {
+        
+    	
+    	User user2 = new User("anonymous", "haidartf@gmail.com", "123456", Role.USER,null);
+       
+       String jwt = jwtService.generateToken(user2);
+       
+       Mockito.when(userDetailsService.loadUserByUsername(user2.getUsername())).thenReturn(null);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/test")
+        		.header("Authorization", "Bearer " + jwt))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+    
+    @Test
+    public void testWithNoHeader() throws Exception {
+
+    	
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/test"))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+}
