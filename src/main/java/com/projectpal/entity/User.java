27cbec8e56a90350a453143084f43576d1b1dddd
haidtf <haidartfeily25@gmail.com>
2023-05-01 22:01:09 +0300

    removed unnecessary imports

--- src/main/java/com/projectpal/entity/User.java
+++ src/main/java/com/projectpal/entity/User.java
@@ -8,12 +8,14 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+
 import com.projectpal.entity.enums.Role;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
