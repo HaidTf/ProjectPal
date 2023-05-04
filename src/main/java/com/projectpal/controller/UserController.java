3196168ef292af24fab54c87aa49ce69c226bcbb
haidtf <haidartfeily25@gmail.com>
2023-05-04 15:29:35 +0300

    modified initially written methods and implemented one method
    modified methods to return the correct http status and return type and implemented addUserToProject method

--- src/main/java/com/projectpal/controller/UserController.java
+++ src/main/java/com/projectpal/controller/UserController.java
@@ -3,57 +3,96 @@ package com.projectpal.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projectpal.entity.User;
+import com.projectpal.exception.BadRequestException;
+import com.projectpal.exception.ConflictException;
+import com.projectpal.exception.ResourceNotFoundException;
 import com.projectpal.repository.UserRepository;
-import com.projectpal.service.SecurityContextService;
+import com.projectpal.utils.ProjectUtil;
+import com.projectpal.utils.SecurityContextUtil;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
 	@Autowired
-	public UserController(SecurityContextService service, UserRepository userRepo,PasswordEncoder encoder) {
-		this.service = service;
+	public UserController(UserRepository userRepo, PasswordEncoder encoder) {
 		this.encoder = encoder;
 		this.userRepo = userRepo;
 	}
 
 	private final PasswordEncoder encoder;
-	
-	private final SecurityContextService service;
-	
+
 	private final UserRepository userRepo;
 	
-	@GetMapping("")
-	public ResponseEntity<User> getUser(){
-		return ResponseEntity.ok(service.getUser());
-	}
 	
-	@PutMapping("/email")
-	public ResponseEntity<String> updateEmail(String email){
-		User user = service.getUser();
+
+	@GetMapping("")
+	public ResponseEntity<User> getUser() {
+		User user = SecurityContextUtil.getUser();
+		user.setPassword(null);
+		return ResponseEntity.ok(user);
+	}
+
+	@PatchMapping("/email")
+	@Transactional
+	public ResponseEntity<Void> updateEmail(@RequestBody String email) {
+		if (email == null)
+			throw new BadRequestException("email is null");
+		User user = SecurityContextUtil.getUser();
 		user.setEmail(email);
 		userRepo.save(user);
-		return ResponseEntity.ok(email);
+		return ResponseEntity.status(204).build();
 	}
-	@PutMapping("/password")
-	public ResponseEntity<String> updatePassword(String password){
-		User user = service.getUser();
+
+	@PatchMapping("/password")
+	@Transactional
+	public ResponseEntity<Void> updatePassword(@RequestBody String password) {
+		if (password == null)
+			throw new BadRequestException("password is null");
+		User user = SecurityContextUtil.getUser();
 		user.setPassword(encoder.encode(password));
 		userRepo.save(user);
-		return ResponseEntity.noContent().build();
+		return ResponseEntity.status(204).build();
 	}
-	
+
 	@DeleteMapping("/delete")
-	public ResponseEntity<Boolean> deleteUser(){
-		userRepo.delete(service.getUser());
-		return ResponseEntity.ok(true);
+	@Transactional
+	public ResponseEntity<Void> deleteUser() {
+		userRepo.delete(SecurityContextUtil.getUser());
+		return ResponseEntity.status(204).build();
 	}
 	
+	@PatchMapping("/addtoproject")
+	@Transactional
+	public ResponseEntity<Void> addUserToProject(@RequestParam String name){
+		
+		ProjectUtil.onlyProjectOwnerAllowed();
+		
+		if (name == null)
+			throw new BadRequestException("the name you typed is null");
+		
+		User toAddUser = userRepo.findUserByName(name).orElseThrow(()-> new ResourceNotFoundException("the User you requested is not found"));
+		
+		if (toAddUser.getProject() != null)
+			throw new ConflictException("User is already in a project");
+		
+		toAddUser.setProject(ProjectUtil.getProjectNotNull());
+		
+		userRepo.save(toAddUser);
+		
+		return ResponseEntity.status(204).build();
+		
+		
+	}
+
 }
