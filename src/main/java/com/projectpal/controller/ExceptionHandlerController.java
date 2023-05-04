6f288c46d47d0d30ae97cd06090e918c2710f4fc
haidtf <haidartfeily25@gmail.com>
2023-05-04 15:24:14 +0300

    changed http status to explicit number

--- src/main/java/com/projectpal/controller/ExceptionHandlerController.java
+++ src/main/java/com/projectpal/controller/ExceptionHandlerController.java
@@ -1,17 +1,37 @@
 package com.projectpal.controller;
 
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.projectpal.exception.BadRequestException;
+import com.projectpal.exception.ForbiddenException;
+import com.projectpal.exception.ResourceNotFoundException;
 
 @ControllerAdvice("com.projectpal.controller")
 public class ExceptionHandlerController {
 
 	@ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+        return ResponseEntity.status(500).body(ex.getMessage());
     }
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex){
+		return ResponseEntity.status(404).body(ex.getMessage());
+	}
+	
+	@ExceptionHandler(BadRequestException.class)
+	public ResponseEntity<String> handleBadRequestException(BadRequestException ex){
+		return ResponseEntity.status(400).body(ex.getMessage());
+	}
+	
+	@ExceptionHandler(ForbiddenException.class)
+	public ResponseEntity<String> handleForbiddenException(ForbiddenException ex) {
+		return ResponseEntity.status(403).body(ex.getMessage());
+	}
+	
 
 	
 }
