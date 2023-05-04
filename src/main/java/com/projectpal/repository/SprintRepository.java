46883bd574d92e16cf4cc6bf2cc1c7ea4e4a9873
haidtf <haidartfeily25@gmail.com>
2023-05-04 15:30:15 +0300

    added findAllByProject method

--- src/main/java/com/projectpal/repository/SprintRepository.java
+++ src/main/java/com/projectpal/repository/SprintRepository.java
@@ -1,10 +1,16 @@
 package com.projectpal.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.projectpal.entity.Project;
 import com.projectpal.entity.Sprint;
 @Repository
 public interface SprintRepository extends JpaRepository<Sprint,Long>{
 
+	Optional<List<Sprint>> findAllByProject(Project project);
+	
 }
