1950b35fa5a6d9033427a6a17b0c7bf5cc18c3b6
haidtf <haidartfeily25@gmail.com>
2023-05-04 15:20:52 +0300

    modified comment

--- src/main/java/com/projectpal/utils/ProjectUtil.java
+++ src/main/java/com/projectpal/utils/ProjectUtil.java
@@ -1,17 +1,32 @@
 package com.projectpal.utils;
 
+import com.projectpal.entity.Project;
 import com.projectpal.entity.User;
 import com.projectpal.exception.ForbiddenException;
+import com.projectpal.exception.ResourceNotFoundException;
 
 public class ProjectUtil {
 
-	//This method is only made for use in com.projectpal.controller package because of the ExceptionHandlerController that handles the exception
+	//These method is only made for use in com.projectpal.controller package because of the ExceptionHandlerController that handles the exception
+	
 	public static void onlyProjectOwnerAllowed() {
 		
 		User mustBeProjectOwner = SecurityContextUtil.getUser();
 
+		if (mustBeProjectOwner.getProject()==null)
+			throw new ResourceNotFoundException("no project is found");
+		
 		if (mustBeProjectOwner.getId() != mustBeProjectOwner.getProject().getOwner().getId())
 			throw new ForbiddenException("you are not the project owner");
+	}
+	
+	public static Project getProjectNotNull() {
+		Project project = SecurityContextUtil.getUser().getProject();
+		
+		if(project == null)
+			throw new ResourceNotFoundException("no project is found");
+		
+		return project;
 	}
 
 }
