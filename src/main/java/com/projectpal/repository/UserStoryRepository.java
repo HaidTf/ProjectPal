7de04cd87e7d3fb4ea2f0fbbb90dc8380372678b
haidtf <haidartfeily25@gmail.com>
2023-05-04 15:30:43 +0300

    added findAllBySprint and findAllByEpic methods

--- src/main/java/com/projectpal/repository/UserStoryRepository.java
+++ src/main/java/com/projectpal/repository/UserStoryRepository.java
@@ -1,10 +1,19 @@
 package com.projectpal.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.projectpal.entity.Epic;
+import com.projectpal.entity.Sprint;
 import com.projectpal.entity.UserStory;
 @Repository
 public interface UserStoryRepository extends JpaRepository<UserStory,Long>{
 
+	Optional<List<UserStory>> findAllByEpic(Epic epic);
+	
+	Optional<List<UserStory>> findAllBySprint(Sprint sprint);
+	
 }
