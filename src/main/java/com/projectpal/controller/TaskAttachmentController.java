366b1bfb9f97d972a5697c20273075eae4437da4
haidtf <haidartfeily25@gmail.com>
2023-05-06 18:04:49 +0300

    added constructor

--- src/main/java/com/projectpal/controller/TaskAttachmentController.java
+++ src/main/java/com/projectpal/controller/TaskAttachmentController.java
@@ -1,10 +1,19 @@
 package com.projectpal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.projectpal.repository.TaskAttachmentRepository;
+import com.projectpal.utils.SecurityContextUtil;
 
 @RestController
 @RequestMapping("/attachment")
 public class TaskAttachmentController {
+	@Autowired
+	public TaskAttachmentController( TaskAttachmentRepository attachmentRepo) {
+		this.attachmentRepo = attachmentRepo;
+	}
 
+	private final TaskAttachmentRepository attachmentRepo;
 }
