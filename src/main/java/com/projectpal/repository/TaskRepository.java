3e21764f53707aef23d1d89b38e0866681dbced3
haidtf <haidartfeily25@gmail.com>
2023-05-06 10:56:41 +0300

    added two methods to find List<Task>
    findAllByUser(User user) , findAllByUserStoryId(long id)

--- src/main/java/com/projectpal/repository/TaskRepository.java
+++ src/main/java/com/projectpal/repository/TaskRepository.java
@@ -1,10 +1,19 @@
 package com.projectpal.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projectpal.entity.Task;
+import com.projectpal.entity.User;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task,Long>{
 
+	Optional<List<Task>> findAllByUser(User user);
+	
+	Optional<List<Task>> findAllByUserStoryId(long id);
+	
 }
