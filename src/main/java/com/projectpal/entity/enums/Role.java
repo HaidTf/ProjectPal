d508e88cb19706e7534ed4a1cc04e6f62a2a640e
haidtf <haidartfeily25@gmail.com>
2023-05-08 19:31:33 +0300

    renamed Roles.java enum to Role.java

---
+++ src/main/java/com/projectpal/entity/enums/Role.java
@@ -0,0 +1,9 @@
+package com.projectpal.entity.enums;
+
+public enum Role {
+
+	ROLE_ADMIN, ROLE_USER_PROJECT_OWNER, ROLE_USER_PROJECT_OPERATOR, ROLE_USER_PROJECT_PARTICIPATOR, ROLE_USER
+
+	//Level of Authority decreases ->
+	
+}
--- src/main/java/com/projectpal/entity/enums/Roles.java
+++
@@ -1,7 +0,0 @@
-package com.projectpal.entity.enums;
-
-public enum Roles {
-
-	ADMIN, USER, USER_PROJECT_OWNER, USER_PROJECT_OPERATOR, USER_PROJECT_PARTICIPATOR
-
-}
