712924523574997ae66b6c6d84eb4d217c686518
haidtf <haidartfeily25@gmail.com>
2023-05-10 14:57:37 +0300

    added constructor and issueDate property

--- src/main/java/com/projectpal/entity/Announcement.java
+++ src/main/java/com/projectpal/entity/Announcement.java
@@ -1,18 +1,26 @@
 package com.projectpal.entity;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 public class Announcement {
 
+	@JsonCreator
 	public Announcement(String title, String description) {
 		this.title = title;
 		this.description = description;
+		this.issueDate = LocalDate.now();
 	}
 
 	@Id
@@ -22,6 +30,9 @@ public class Announcement {
 	private String title;
 	
 	private String description;
+	
+	@Temporal(TemporalType.DATE)
+	private LocalDate issueDate;
 	
 	@ManyToOne
 	private Project project;
@@ -48,5 +59,13 @@ public class Announcement {
 
 	public void setProject(Project project) {
 		this.project = project;
+	}
+
+	public LocalDate getIssueDate() {
+		return issueDate;
+	}
+
+	public void setIssueDate(LocalDate issueDate) {
+		this.issueDate = issueDate;
 	}
 }
