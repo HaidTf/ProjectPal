d2ef6b1c7033e127e000c014d13da26651aa10a4
haidtf <haidartfeily25@gmail.com>
2023-05-10 14:57:10 +0300

    added constructor and issueDate property

--- src/main/java/com/projectpal/entity/Invitation.java
+++ src/main/java/com/projectpal/entity/Invitation.java
@@ -1,31 +1,44 @@
 package com.projectpal.entity;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 public class Invitation {
 
+	public Invitation(User invitedUser, Project project) {
+		this.invitedUser = invitedUser;
+		this.project = project;
+		this.issueDate = LocalDate.now();
+	}
+
 	@Id
 	@GeneratedValue(generator = "ID_GENERATOR")
 	private long id;
 	
+	@Temporal(TemporalType.DATE)
+	private LocalDate issueDate;
+	
 	@ManyToOne
-	private User user;
+	private User invitedUser;
 	
 	@ManyToOne
 	private Project project;
 
 	public User getUser() {
-		return user;
+		return invitedUser;
 	}
 
 	public void setUser(User user) {
-		this.user = user;
+		this.invitedUser = user;
 	}
 
 	public Project getProject() {
@@ -34,6 +47,14 @@ public class Invitation {
 
 	public void setProject(Project project) {
 		this.project = project;
+	}
+
+	public LocalDate getIssueDate() {
+		return issueDate;
+	}
+
+	public void setIssueDate(LocalDate issueDate) {
+		this.issueDate = issueDate;
 	}
 	
 }
