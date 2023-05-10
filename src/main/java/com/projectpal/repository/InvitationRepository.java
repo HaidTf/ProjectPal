58c95c923b10c57adbe93759d85adc729219e32a
haidtf <haidartfeily25@gmail.com>
2023-05-10 14:54:45 +0300

    added two methods to retrieve invitations based on user and project

--- src/main/java/com/projectpal/repository/InvitationRepository.java
+++ src/main/java/com/projectpal/repository/InvitationRepository.java
@@ -1,11 +1,20 @@
 package com.projectpal.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projectpal.entity.Invitation;
+import com.projectpal.entity.Project;
+import com.projectpal.entity.User;
 
 @Repository
 public interface InvitationRepository extends JpaRepository<Invitation,Long> {
 
+	Optional<List<Invitation>> findAllByInvitedUser(User invitedUser);
+
+	Optional<List<Invitation>> findAllByProject(Project project);
+	
 }
