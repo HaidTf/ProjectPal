fc3b1da8464d066945648346d0fb6ce779653899
haidtf <haidartfeily25@gmail.com>
2023-05-10 14:55:06 +0300

    added findAllByProject() method

--- src/main/java/com/projectpal/repository/AnnouncementRepository.java
+++ src/main/java/com/projectpal/repository/AnnouncementRepository.java
@@ -1,11 +1,17 @@
 package com.projectpal.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projectpal.entity.Announcement;
+import com.projectpal.entity.Project;
 
 @Repository
 public interface AnnouncementRepository extends JpaRepository<Announcement,Long>{
+
+	Optional<List<Announcement>> findAllByProject(Project project);
 
 }
