fbf8b8d2566a71a618520100ed4ba19a2c2102f3
haidtf <haidartfeily25@gmail.com>
2023-05-10 14:59:20 +0300

    added setProjectOperator and removeUserFromProject methods

--- src/main/java/com/projectpal/controller/ProjectController.java
+++ src/main/java/com/projectpal/controller/ProjectController.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,10 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.projectpal.entity.Project;
 import com.projectpal.entity.User;
+import com.projectpal.entity.enums.Role;
 import com.projectpal.exception.BadRequestException;
+import com.projectpal.exception.ForbiddenException;
+import com.projectpal.exception.ResourceNotFoundException;
 import com.projectpal.repository.ProjectRepository;
 import com.projectpal.repository.UserRepository;
 import com.projectpal.utils.ProjectUtil;
@@ -76,13 +80,55 @@ public class ProjectController {
 	public ResponseEntity<Void> updateDescription(@RequestParam String description) {
 
 		Project project = ProjectUtil.getProjectNotNull();
-		
+
 		project.setDescription(description);
 
 		projectRepo.save(project);
 
 		return ResponseEntity.status(204).build();
 
+	}
+	@PreAuthorize("hasRole('USER_PROJECT_OWNER')")
+	@PatchMapping("/update/setoperator/{name}")
+	@Transactional
+	public ResponseEntity<Void> setProjectOperator(@PathVariable String name){
+		
+		User user = userRepo.findUserByName(name)
+				.orElseThrow(() -> new ResourceNotFoundException("no user with the intended name is found"));
+
+		if (user.getProject().getId() != ProjectUtil.getProjectNotNull().getId())
+			throw new ForbiddenException("you are not allowed remove users from other projects");
+
+		if (SecurityContextUtil.getUser().getName() == name)
+			throw new BadRequestException("you cant set yourself as a project operator");
+		
+		user.setRole(Role.ROLE_USER_PROJECT_OPERATOR);
+		
+		userRepo.save(user);
+		
+		return ResponseEntity.status(204).build();
+		
+	}
+
+	@PreAuthorize("hasAnyRole('USER_PROJECT_OWNER','USER_PROJECT_OPERATOR')")
+	@PatchMapping("/remove/user/{name}")
+	@Transactional
+	public ResponseEntity<Void> removeUserFromProject(@PathVariable String name) {
+
+		User user = userRepo.findUserByName(name)
+				.orElseThrow(() -> new ResourceNotFoundException("no user with the intended name is found"));
+
+		if (user.getProject().getId() != ProjectUtil.getProjectNotNull().getId())
+			throw new ForbiddenException("you are not allowed remove users from other projects");
+
+		if (SecurityContextUtil.getUser().getName() == name)
+			throw new BadRequestException("you cant remove yourself from the project through here");
+		
+		user.setProject(null);
+
+		userRepo.save(user);
+
+		return ResponseEntity.status(204).build();
 	}
 
 	@PreAuthorize("hasRole('USER_PROJECT_OWNER')")
@@ -93,7 +139,7 @@ public class ProjectController {
 		Project project = ProjectUtil.getProjectNotNull();
 
 		projectRepo.delete(project);
-		
+
 		return ResponseEntity.status(204).build();
 
 	}
