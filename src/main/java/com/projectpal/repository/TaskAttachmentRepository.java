57dc04fccf75113d5a5737f3641e04b04b7294e1
haidtf <haidartfeily25@gmail.com>
2023-05-11 14:51:43 +0300

    added findAllByTask method

--- src/main/java/com/projectpal/repository/TaskAttachmentRepository.java
+++ src/main/java/com/projectpal/repository/TaskAttachmentRepository.java
@@ -1,10 +1,18 @@
 package com.projectpal.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+import com.projectpal.entity.Task;
 import com.projectpal.entity.TaskAttachment;
+
 @Repository
 public interface TaskAttachmentRepository extends JpaRepository<TaskAttachment,Long> {
+
+	Optional<List<TaskAttachment>> findAllByTask(Task task);
 
 }
