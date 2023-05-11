ab9adee2ce3f904b830ef72bbe29dedba471d511
haidtf <haidartfeily25@gmail.com>
2023-05-11 14:54:10 +0300

    implemented FileStorageService to store files on local file system

---
+++ src/main/java/com/projectpal/service/FileStorageService.java
@@ -0,0 +1,69 @@
+package com.projectpal.service;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.projectpal.exception.BadRequestException;
+import com.projectpal.exception.InternalServerErrorException;
+import com.projectpal.exception.ResourceNotFoundException;
+
+@Service
+public class FileStorageService {
+
+	@Value("${file.upload-dir}")
+	private String storageLocation;
+
+	public String storeFile(MultipartFile file, long parentId) {
+
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+		try {
+			if (fileName.contains(".."))
+				throw new BadRequestException("invalid characters found");
+
+			String parentDirectoryPath = storageLocation + "/" + parentId;
+
+			Files.createDirectories(Path.of(parentDirectoryPath));
+
+			Path filePath = Path.of(parentDirectoryPath, fileName);
+
+			Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+			return fileName;
+
+		} catch (IOException ex) {
+			throw new InternalServerErrorException("could not store file");
+		}
+		
+	}
+
+	public Resource loadFile(String fileName,long parentId) {
+
+		try {
+			String parentDirectoryPath = storageLocation + "/" + parentId;
+			
+			Path filePath = Path.of(parentDirectoryPath, fileName);
+			
+			Resource resource = new UrlResource(filePath.toUri());
+			
+			if (resource.exists()) {
+				return resource;
+			} else {
+				throw new ResourceNotFoundException("file not found");
+			}
+		} catch (MalformedURLException ex) {
+			throw new BadRequestException("invalid file path");
+		}
+	}
+
+}
