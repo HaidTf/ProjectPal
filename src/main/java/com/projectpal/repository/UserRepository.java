49e50a9d624363e893ba45db94c4f041e92ac6a3
haidtf <haidartfeily25@gmail.com>
2023-05-15 15:24:01 +0300

    added findAllByProject method

--- src/main/java/com/projectpal/repository/UserRepository.java
+++ src/main/java/com/projectpal/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.projectpal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.projectpal.entity.Project;
 import com.projectpal.entity.User;
 
 @Repository
@@ -13,4 +15,6 @@ public interface UserRepository extends JpaRepository<User,Long> {
 	Optional<User> findUserByEmail(String email);
 	
 	Optional<User> findUserByName(String name);
+
+	Optional<List<User>> findAllByProject(Project project);
 }
