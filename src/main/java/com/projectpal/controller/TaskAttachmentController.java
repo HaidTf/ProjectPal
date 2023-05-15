13dc7e87bbb16609c7f1fa6da16b42a2a55c225d
haidtf <haidartfeily25@gmail.com>
2023-05-15 15:14:29 +0300

    added deleteFile method

--- src/main/java/com/projectpal/controller/TaskAttachmentController.java
+++ src/main/java/com/projectpal/controller/TaskAttachmentController.java
@@ -7,6 +7,8 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +49,7 @@ public class TaskAttachmentController {
 
 	@PreAuthorize("hasAnyRole('USER_PROJECT_OWNER','USER_PROJECT_OPERATOR')")
 	@PostMapping("/upload")
+	@Transactional
 	public ResponseEntity<Void> uploadFile(@RequestParam MultipartFile file, @RequestParam long taskId) {
 
 		if (file.isEmpty())
@@ -102,4 +105,27 @@ public class TaskAttachmentController {
 				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
 				.body(resource);
 	}
+	
+	@DeleteMapping("/delete/{taskId}/{fileName}")
+	@Transactional
+	public ResponseEntity<Void> deleteFile(@PathVariable long taskId , @PathVariable String fileName){
+		
+		Task task = taskRepo.findById(taskId)
+				.orElseThrow(() -> new ResourceNotFoundException("no task with this id is found"));
+
+		if (task.getProject().getId() != ProjectUtil.getProjectNotNull().getId())
+			throw new ForbiddenException("you are not allowed to access other projects");
+		
+		TaskAttachment attachment = attachmentRepo.findTaskAttachmentByFileName(fileName).orElseThrow(() -> new ResourceNotFoundException("no taskAttachment with this name is found"));;
+		
+		if(attachment.getTask().getId()!=task.getId())
+			throw new ForbiddenException("you are not allowed to access other projects");
+		
+		attachmentRepo.delete(attachment);
+		
+		fileStorageService.deleteFile(fileName, taskId);
+		
+		return ResponseEntity.status(204).build();
+	}
+	
 }
