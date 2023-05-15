bf799d6e6451577d7e9b7f1f6b5940709123a39f
haidtf <haidartfeily25@gmail.com>
2023-05-15 15:14:01 +0300

    updated deleteProject and removeUserFromProject methods modified removeUserFromProject to set the assigned user of the tasks assigned to the removed user to null modified deleteProject to set the roles of the deleted project users to Role.ROLE_USER

--- src/main/java/com/projectpal/controller/ProjectController.java
+++ src/main/java/com/projectpal/controller/ProjectController.java
@@ -2,6 +2,7 @@ package com.projectpal.controller;
 
 import java.net.URI;
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +21,14 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.projectpal.entity.Project;
+import com.projectpal.entity.Task;
 import com.projectpal.entity.User;
 import com.projectpal.entity.enums.Role;
 import com.projectpal.exception.BadRequestException;
 import com.projectpal.exception.ForbiddenException;
 import com.projectpal.exception.ResourceNotFoundException;
 import com.projectpal.repository.ProjectRepository;
+import com.projectpal.repository.TaskRepository;
 import com.projectpal.repository.UserRepository;
 import com.projectpal.utils.ProjectUtil;
 import com.projectpal.utils.SecurityContextUtil;
@@ -35,15 +38,18 @@ import com.projectpal.utils.SecurityContextUtil;
 public class ProjectController {
 
 	@Autowired
-	public ProjectController(ProjectRepository projectRepo, UserRepository userRepo) {
+	public ProjectController(ProjectRepository projectRepo, UserRepository userRepo, TaskRepository taskRepo) {
 
 		this.projectRepo = projectRepo;
 		this.userRepo = userRepo;
+		this.taskRepo = taskRepo;
 	}
 
 	private final ProjectRepository projectRepo;
 
 	private final UserRepository userRepo;
+
+	private final TaskRepository taskRepo;
 
 	@GetMapping("")
 	public ResponseEntity<Project> getProject() {
@@ -51,7 +57,7 @@ public class ProjectController {
 		Project project = ProjectUtil.getProjectNotNull();
 
 		project.setLastAccessedDate(LocalDate.now());
-		
+
 		return ResponseEntity.ok(project);
 	}
 
@@ -67,7 +73,7 @@ public class ProjectController {
 
 		user.setRole(Role.ROLE_USER_PROJECT_OWNER);
 		user.setProject(project);
-		
+
 		project.setOwner(user);
 
 		projectRepo.save(project);
@@ -93,26 +99,27 @@ public class ProjectController {
 		return ResponseEntity.status(204).build();
 
 	}
+
 	@PreAuthorize("hasRole('USER_PROJECT_OWNER')")
 	@PatchMapping("/update/setoperator/{name}")
 	@Transactional
-	public ResponseEntity<Void> setProjectOperator(@PathVariable String name){
-		
+	public ResponseEntity<Void> setProjectOperator(@PathVariable String name) {
+
 		User user = userRepo.findUserByName(name)
 				.orElseThrow(() -> new ResourceNotFoundException("no user with the intended name is found"));
 
 		if (user.getProject().getId() != ProjectUtil.getProjectNotNull().getId())
-			throw new ForbiddenException("you are not allowed remove users from other projects");
+			throw new ForbiddenException("the user must be in the project to be set as project operator");
 
 		if (SecurityContextUtil.getUser().getName() == name)
 			throw new BadRequestException("you cant set yourself as a project operator");
-		
+
 		user.setRole(Role.ROLE_USER_PROJECT_OPERATOR);
-		
+
 		userRepo.save(user);
-		
+
 		return ResponseEntity.status(204).build();
-		
+
 	}
 
 	@PreAuthorize("hasAnyRole('USER_PROJECT_OWNER','USER_PROJECT_OPERATOR')")
@@ -128,11 +135,21 @@ public class ProjectController {
 
 		if (SecurityContextUtil.getUser().getName() == name)
 			throw new BadRequestException("you cant remove yourself from the project through here");
-		
+
 		user.setProject(null);
 
 		userRepo.save(user);
 
+		List<Task> tasks = taskRepo.findAllByAssignedUser(user).orElse(null);
+
+		if (tasks != null) {
+
+			for (Task task : tasks) {
+				task.setAssignedUser(null);
+				taskRepo.save(task);
+			}
+
+		}
 		return ResponseEntity.status(204).build();
 	}
 
@@ -143,6 +160,17 @@ public class ProjectController {
 
 		Project project = ProjectUtil.getProjectNotNull();
 
+		List<User> projectUsers = userRepo.findAllByProject(project).orElse(null);
+
+		if (projectUsers != null) {
+
+			for (User projectUser : projectUsers) {
+				projectUser.setRole(Role.ROLE_USER);
+			}
+			
+		}
+		
+		SecurityContextUtil.getUser().setRole(Role.ROLE_USER);
 		projectRepo.delete(project);
 
 		return ResponseEntity.status(204).build();
