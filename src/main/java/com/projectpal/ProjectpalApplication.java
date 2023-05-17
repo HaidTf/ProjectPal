cebc34d7d051e249235bf1e2440a328b08ed0e8c
haidtf <haidartfeily25@gmail.com>
2023-05-17 15:14:12 +0300

    added @EnableCaching annotation

--- src/main/java/com/projectpal/ProjectpalApplication.java
+++ src/main/java/com/projectpal/ProjectpalApplication.java
@@ -2,12 +2,14 @@ package com.projectpal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.projectpal.repository")
 @EnableScheduling
+@EnableCaching
 public class ProjectpalApplication {
 
 	public static void main(String[] args) {
