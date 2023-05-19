5bf009961f5acb5ea657c54eadd964da55318d54
haidtf <haidartfeily25@gmail.com>
2023-05-19 19:07:58 +0300

    added methods to update user email, password and role , also added findAllByRole method

--- src/main/java/com/projectpal/repository/UserRepository.java
+++ src/main/java/com/projectpal/repository/UserRepository.java
@@ -3,18 +3,35 @@ package com.projectpal.repository;
 import java.util.List;
 import java.util.Optional;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.projectpal.entity.Project;
 import com.projectpal.entity.User;
+import com.projectpal.entity.enums.Role;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findUserByEmail(String email);
-	
+
 	Optional<User> findUserByName(String name);
 
 	Optional<List<User>> findAllByProject(Project project);
+
+	Optional<List<User>> findAllByRole(Role role);
+
+	@Modifying
+	@Query("UPDATE User u SET u.email = ?2 WHERE u.id = ?1")
+	void updateEmailById(Long id, String email);
+
+	@Modifying
+	@Query("UPDATE User u SET u.password = ?2 WHERE u.id = ?1")
+	void updatePasswordById(Long id, String password);
+
+	@Modifying
+	@Query("UPDATE User u SET u.role = ?2 WHERE u.id = ?1")
+	void updateRoleById(Long id, Role role);
+
 }
