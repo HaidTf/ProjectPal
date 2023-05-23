548b66988c437fa34baebb86e9b61646ca86f5e7
haidtf <haidartfeily25@gmail.com>
2023-05-23 15:23:21 +0300

    added generic addObjectToCache, generic deleteObjectFromCache and evictListFromCache methods

--- src/main/java/com/projectpal/service/CacheService.java
+++ src/main/java/com/projectpal/service/CacheService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,8 @@ import com.projectpal.repository.EpicRepository;
 import com.projectpal.repository.SprintRepository;
 import com.projectpal.repository.UserStoryRepository;
 
+import jakarta.annotation.PostConstruct;
+
 @Service
 public class CacheService {
 
@@ -30,6 +33,15 @@ public class CacheService {
 		this.redis = redis;
 	}
 
+	@PostConstruct
+	private void clearCache() {
+		try {
+			redis.getCacheNames().forEach(cacheName -> redis.getCache(cacheName).clear());
+		} catch(Exception ex) {
+			
+		}
+	}
+	
 	private final RedisCacheManager redis;
 
 	private final EpicRepository epicRepo;
@@ -52,14 +64,16 @@ public class CacheService {
 	// name of cache
 	// cache key
 	// R : Repository of the cached object T
-	// Function to be applied on the Repository to find the list of Objects of type T
-	
+	// Function to be applied on the Repository to find the list of Objects of type
+	// T
 
 	// #Method Explanation:
 	// 1) Tries to get cache using cache name (cacheName) and cache key (cacheKey),
-	// if the method is successful and value is found then the remaining of the method is NOT executed
+	// if the method is successful and value is found then the remaining of the
+	// method is NOT executed
 	// 2) If an exception is thrown then the list is set to null
-	// 3) If list is Null or Empty then the list is queried from the database and put into cache
+	// 3) If list is Null or Empty then the list is queried from the database and
+	// put into cache
 	// 4) List is returned
 
 	public <T, R extends JpaRepository<T, Long>> List<T> getCachedObjects(String cacheName, Long cacheKey, R repository,
@@ -75,12 +89,44 @@ public class CacheService {
 		if (objects == null || objects.isEmpty()) {
 
 			objects = findAllByParentId.apply(repository)
-					.orElseThrow(() -> new ResourceNotFoundException("no epics found"));
+					.orElseThrow(() -> new ResourceNotFoundException("no entities found"));
 
 			redis.getCache(cacheName).put(cacheKey, objects);
 		}
 
 		return objects;
+	}
+	
+	// Generic Add Object To Cache Method
+
+	// #Method Parameters:
+	// name of cache
+	// cache key
+	// Object T : to be added object
+
+	// Method Explanation:
+	// 1) Tries to get cache using cache name (cacheName) and cache key (cacheKey)
+	// 2) If an exception is thrown then the cache is evicted
+	// 3) If List is Found and not empty then object T is added to it
+	// 4) List is put into cache to overwrite the invalid cache
+
+	public <T> void addObjectToCache(String cacheName, Long cacheKey, T object) {
+
+		List<T> objects;
+
+		try {
+			objects = redis.getCache(cacheName).get(cacheKey, List.class);
+
+			if (objects != null) {
+				objects.add(object);
+				redis.getCache(cacheName).put(cacheKey, objects);
+			}
+		} catch (Exception ex) {
+			Cache cache = redis.getCache(cacheName);
+			if (cache != null)
+				cache.evictIfPresent(cacheKey);
+
+		}
 	}
 
 	// Generic Update Property Method
@@ -89,16 +135,19 @@ public class CacheService {
 	// name of cache
 	// cache key
 	// Object T : to be updated object
-	// Function to be applied on object T to update its property, e.g: (user)-> user.setName("..")
+	// Function to be applied on object T to get its id
+	// Function to be applied on object T to update its property, e.g: (user)->
+	// user.setName("..")
 
 	// Method Explanation:
 	// 1) Tries to get cache using cache name (cacheName) and cache key (cacheKey)
 	// 2) If an exception is thrown then the cache is evicted
-	// 3) If List is Found and not empty then it is searched for the ToBeUpdated Object T
+	// 3) If List is Found and not empty then it is searched for the ToBeUpdated
+	// Object T
 	// 4) Object T property is updated using Function
 	// 5) List is put into cache to overwrite the invalid cache
 
-	public <P, T> void updateObjectPropertyInCache(String cacheName, Long cacheKey, T object,
+	public <T> void updateObjectPropertyInCache(String cacheName, Long cacheKey, T object,
 			Function<T, Long> getObjectId, Function<T, Void> updateTProperty) {
 
 		List<T> objects;
@@ -117,9 +166,62 @@ public class CacheService {
 				redis.getCache(cacheName).put(cacheKey, objects);
 			}
 		} catch (Exception ex) {
-			redis.getCache(cacheName).evictIfPresent(cacheKey);
+			Cache cache = redis.getCache(cacheName);
+			if (cache != null)
+				cache.evictIfPresent(cacheKey);
 		}
 	}
+
+	// Generic Delete Object From Cache Method
+
+	// #Method Parameters:
+	// name of cache
+	// cache key
+	// Object T : to be Deleted object
+	// Function to be applied on object T to get its id
+
+	// Method Explanation:
+	// 1) Tries to get cache using cache name (cacheName) and cache key (cacheKey)
+	// 2) If an exception is thrown then the cache is evicted
+	// 3) If List is Found and not empty then it is searched for the ToBeDeleted
+	// Object T
+	// 4) Object T is removed from list
+	// 5) List is put into cache to overwrite the invalid cache
+
+	public <T> void deleteObjectFromCache(String cacheName, Long cacheKey, T object, Function<T, Long> getObjectId) {
+
+		List<T> objects;
+
+		try {
+			objects = redis.getCache(cacheName).get(cacheKey, List.class);
+
+			if (objects != null && !objects.isEmpty()) {
+				for (T object2 : objects) {
+					if (getObjectId.apply(object2) == getObjectId.apply(object)) {
+						objects.remove(object2);
+						break;
+					}
+				}
+
+				redis.getCache(cacheName).put(cacheKey, objects);
+			}
+		} catch (Exception ex) {
+			Cache cache = redis.getCache(cacheName);
+			if (cache != null)
+				cache.evictIfPresent(cacheKey);
+
+		}
+	}
+
+	public void evictListFromCache(String cacheName, Long cacheKey) {
+		Cache cache = redis.getCache(cacheName);
+		if (cache != null)
+			cache.evictIfPresent(cacheKey);
+	}
+
+	// getCachedObjects() and updateObjectPropertyInCache() methods are given
+	// distinct
+	// implementations due to their complexity
 
 	// Epic
 
@@ -144,7 +246,7 @@ public class CacheService {
 	}
 
 	public void updateSprintProperty(Sprint sprint, Function<Sprint, Void> updateSprintProperty) {
-		
+
 		this.updateObjectPropertyInCache(sprintListCache, sprint.getProject().getId(), sprint, Sprint::getId,
 				updateSprintProperty);
 	}
@@ -152,22 +254,22 @@ public class CacheService {
 	// UserStory
 
 	public List<UserStory> getCachedEpicUserStoryList(Epic epic) {
-		
+
 		return this.getCachedObjects(epicUserStoryListCache, epic.getId(), userStoryRepo,
 				repo -> repo.findAllByEpicId(epic.getId()));
 	}
 
 	public List<UserStory> getCachedSprintUserStoryList(Sprint sprint) {
-		
+
 		return this.getCachedObjects(sprintUserStoryListCache, sprint.getId(), userStoryRepo,
 				repo -> repo.findAllBySprintId(sprint.getId()));
 	}
 
 	public void updateUserStoryProperty(UserStory userStory, Function<UserStory, Void> updateUserStoryProperty) {
-		
+
 		this.updateObjectPropertyInCache(epicUserStoryListCache, userStory.getEpic().getId(), userStory,
 				UserStory::getId, updateUserStoryProperty);
-		
+
 		this.updateObjectPropertyInCache(sprintUserStoryListCache, userStory.getSprint().getId(), userStory,
 				UserStory::getId, updateUserStoryProperty);
 	}
