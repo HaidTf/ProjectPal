ed914bc9b25c6b9f1266f777d41e27555cb264f9
haidtf <haidartfeily25@gmail.com>
2023-05-24 17:21:13 +0300

    updated epic and sprint caches to use a new config which has a timeToLive of 4 hours

--- src/main/java/com/projectpal/configuration/RedisCacheConfig.java
+++ src/main/java/com/projectpal/configuration/RedisCacheConfig.java
@@ -11,7 +11,9 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
-import com.projectpal.service.CacheService;
+import com.projectpal.service.CacheServiceEpicImpl;
+import com.projectpal.service.CacheServiceSprintImpl;
+import com.projectpal.service.CacheServiceUserStoryImpl;
 
 @Configuration
 @EnableCaching
@@ -20,16 +22,21 @@ public class RedisCacheConfig {
     @Bean
     RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
     	
-        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+        RedisCacheConfiguration cacheConfiguration1 = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(4)) 
+                .disableCachingNullValues() 
+                .computePrefixWith(cacheName -> "projectpal:" + cacheName);
+        
+        RedisCacheConfiguration cacheConfiguration2 = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(30)) 
                 .disableCachingNullValues() 
                 .computePrefixWith(cacheName -> "projectpal:" + cacheName);
 
         Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
-        cacheConfigurations.put(CacheService.epicListCache, cacheConfiguration);
-        cacheConfigurations.put(CacheService.sprintListCache, cacheConfiguration);
-        cacheConfigurations.put(CacheService.epicUserStoryListCache, cacheConfiguration);
-        cacheConfigurations.put(CacheService.sprintUserStoryListCache, cacheConfiguration);
+        cacheConfigurations.put(CacheServiceEpicImpl.epicListCache, cacheConfiguration1);
+        cacheConfigurations.put(CacheServiceSprintImpl.sprintListCache, cacheConfiguration1);
+        cacheConfigurations.put(CacheServiceUserStoryImpl.epicUserStoryListCache, cacheConfiguration2);
+        cacheConfigurations.put(CacheServiceUserStoryImpl.sprintUserStoryListCache, cacheConfiguration2);
 
         RedisCacheManager cacheManager = RedisCacheManager.builder(redisConnectionFactory)
         		.withInitialCacheConfigurations(cacheConfigurations)
