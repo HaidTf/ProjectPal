0a95c8051f1e7a723190760f448190d33be00f05
haidtf <haidartfeily25@gmail.com>
2023-05-24 17:22:53 +0300

    created CacheServiceProjectAddOn class which contains a method to cascade the delete of project to the children entities in cache

---
+++ src/main/java/com/projectpal/service/CacheServiceProjectAddOn.java
@@ -0,0 +1,46 @@
+package com.projectpal.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projectpal.entity.Epic;
+import com.projectpal.entity.Project;
+import com.projectpal.entity.Sprint;
+
+@Service
+public class CacheServiceProjectAddOn {
+
+	@Autowired
+	public CacheServiceProjectAddOn(CacheService cacheService, CacheServiceEpicImpl cacheServiceEpicImpl,
+			CacheServiceSprintImpl cacheServiceSprintImpl) {
+		this.cacheService = cacheService;
+		this.cacheServiceEpicImpl = cacheServiceEpicImpl;
+		this.cacheServiceSprintImpl = cacheServiceSprintImpl;
+	}
+	
+	private final CacheService cacheService;
+	
+	private final CacheServiceEpicImpl cacheServiceEpicImpl;
+	
+	private final CacheServiceSprintImpl cacheServiceSprintImpl;
+	
+	// Project Deletion -> Cascade Remove of all child entities from cache:
+
+	public void DeleteEntitiesInCacheOnProjectDeletion(Project project) {
+
+		List<Epic> epics = cacheServiceEpicImpl.getCachedEpicList(project);
+		List<Sprint> sprints = cacheServiceSprintImpl.getCachedSprintList(project);
+
+		for (Epic epic : epics)
+			cacheService.evictListFromCache(CacheServiceUserStoryImpl.epicUserStoryListCache, epic.getId());
+
+		for (Sprint sprint : sprints)
+			cacheService.evictListFromCache(CacheServiceUserStoryImpl.sprintUserStoryListCache, sprint.getId());
+
+		cacheService.evictListFromCache(CacheServiceEpicImpl.epicListCache, project.getId());
+		cacheService.evictListFromCache(CacheServiceSprintImpl.sprintListCache, project.getId());
+	}
+	
+}
