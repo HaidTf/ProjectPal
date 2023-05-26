174404c1033b44cb59f19849ffa4820a5b78dad3
haidtf <haidartfeily25@gmail.com>
2023-05-26 15:52:54 +0300

    modified handleValidationException method to return a list of formatted error strings in GlobalExceptionHandler class

--- src/main/java/com/projectpal/controller/GlobalExceptionHandler.java
+++ src/main/java/com/projectpal/controller/GlobalExceptionHandler.java
@@ -1,10 +1,8 @@
 package com.projectpal.controller;
 
-
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 
 import com.projectpal.exception.BadRequestException;
 import com.projectpal.exception.ForbiddenException;
@@ -20,63 +19,65 @@ import com.projectpal.exception.ResourceNotFoundException;
 
 @ControllerAdvice("com.projectpal.controller")
 public class GlobalExceptionHandler {
-	
-	//Default Exception Object
+
+	// Default Exception Object
 	@ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
+	public ResponseEntity<String> handleException(Exception ex) {
 		ex.printStackTrace(System.out);
-        return ResponseEntity.status(500).body(ex.getMessage());
-    }
-	
-	//Raised when null request is received into a @RequestBody annotated method parameter
-	
+		return ResponseEntity.status(500).body(ex.getMessage());
+	}
+
+	// Raised when null request is received into a @RequestBody annotated method
+	// parameter
+
 	@ExceptionHandler(HttpMessageNotReadableException.class)
-	public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex){
+	public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
 		return ResponseEntity.status(400).body(ex.getMessage());
 	}
-	
-	//Raised when a bean validation error occurs due to @Valid annotation
-	
+
+	// Raised when a bean validation error occurs due to @Valid annotation
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<String>> handleValidationException(MethodArgumentNotValidException ex) {
-		
-        BindingResult result = ex.getBindingResult();
-        
-        List<String> errors = result.getAllErrors().stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.toList());
-        
-        return ResponseEntity.status(400).body(errors);
-    }
-	
-	//Raised when @PreAuthorize annotation denies access
-	
+	public ResponseEntity<List<String>> handleValidationException(MethodArgumentNotValidException ex) {
+
+		BindingResult result = ex.getBindingResult();
+
+		List<ObjectError> errors = result.getAllErrors();
+
+		List<String> stringErrors = new ArrayList<>(errors.size());
+
+		for (ObjectError err : errors)
+			stringErrors.add(err.getCodes()[0] + " " + err.getDefaultMessage());
+
+		return ResponseEntity.status(400).body(stringErrors);
+	}
+
+	// Raised when @PreAuthorize annotation denies access
+
 	@ExceptionHandler(AccessDeniedException.class)
-	public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex){
+	public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex) {
 		return ResponseEntity.status(403).body(ex.getMessage());
 	}
-	
-	//Raised Explicitly
-	
+
+	// Raised Explicitly
+
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex){
+	public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
 		return ResponseEntity.status(404).body(ex.getMessage());
 	}
-	
-	//Raised Explicitly
-	
+
+	// Raised Explicitly
+
 	@ExceptionHandler(BadRequestException.class)
-	public ResponseEntity<String> handleBadRequestException(BadRequestException ex){
+	public ResponseEntity<String> handleBadRequestException(BadRequestException ex) {
 		return ResponseEntity.status(400).body(ex.getMessage());
 	}
-	
-	//Raised Explicitly
-	
+
+	// Raised Explicitly
+
 	@ExceptionHandler(ForbiddenException.class)
 	public ResponseEntity<String> handleForbiddenException(ForbiddenException ex) {
 		return ResponseEntity.status(403).body(ex.getMessage());
 	}
-	
 
-	
 }
