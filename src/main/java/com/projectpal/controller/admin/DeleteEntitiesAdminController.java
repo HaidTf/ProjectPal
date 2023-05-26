2c339685a722d46527838184d60b9f8a4d863307
haidtf <haidartfeily25@gmail.com>
2023-05-26 15:15:25 +0300

    modified methods in DeleteEntitiesAdminController to cascade delete of entities to children entities in cache

--- src/main/java/com/projectpal/controller/admin/DeleteEntitiesAdminController.java
+++ src/main/java/com/projectpal/controller/admin/DeleteEntitiesAdminController.java
@@ -1,5 +1,6 @@
 package com.projectpal.controller.admin;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projectpal.entity.Epic;
+import com.projectpal.entity.Sprint;
+import com.projectpal.entity.UserStory;
+import com.projectpal.entity.Project;
+import com.projectpal.exception.ResourceNotFoundException;
 import com.projectpal.repository.AnnouncementRepository;
 import com.projectpal.repository.EpicRepository;
 import com.projectpal.repository.InvitationRepository;
@@ -16,6 +22,11 @@ import com.projectpal.repository.SprintRepository;
 import com.projectpal.repository.TaskAttachmentRepository;
 import com.projectpal.repository.TaskRepository;
 import com.projectpal.repository.UserStoryRepository;
+import com.projectpal.service.CacheService;
+import com.projectpal.service.CacheServiceEpicImpl;
+import com.projectpal.service.CacheServiceProjectAddOn;
+import com.projectpal.service.CacheServiceSprintImpl;
+import com.projectpal.service.CacheServiceUserStoryImpl;
 
 @RestController
 @RequestMapping("/admin/entities/delete")
@@ -25,8 +36,9 @@ public class DeleteEntitiesAdminController {
 	public DeleteEntitiesAdminController(ProjectRepository projectRepo, EpicRepository epicRepo,
 			SprintRepository sprintRepo, UserStoryRepository userStoryRepo, TaskRepository taskRepo,
 			TaskAttachmentRepository taskAttachmentRepo, InvitationRepository invitationRepo,
-			AnnouncementRepository announcementRepo) {
-
+			AnnouncementRepository announcementRepo, CacheServiceEpicImpl cacheServiceEpicImpl,
+			CacheServiceSprintImpl cacheServiceSprintImpl, CacheService cacheService,
+			CacheServiceProjectAddOn cacheServiceProjectAddOn) {
 		this.projectRepo = projectRepo;
 		this.epicRepo = epicRepo;
 		this.sprintRepo = sprintRepo;
@@ -35,6 +47,10 @@ public class DeleteEntitiesAdminController {
 		this.taskAttachmentRepo = taskAttachmentRepo;
 		this.invitationRepo = invitationRepo;
 		this.announcementRepo = announcementRepo;
+		this.cacheServiceEpicImpl = cacheServiceEpicImpl;
+		this.cacheServiceSprintImpl = cacheServiceSprintImpl;
+		this.cacheService = cacheService;
+		this.cacheServiceProjectAddOn = cacheServiceProjectAddOn;
 	}
 
 	private final ProjectRepository projectRepo;
@@ -53,12 +69,25 @@ public class DeleteEntitiesAdminController {
 
 	private final AnnouncementRepository announcementRepo;
 
+	private final CacheServiceEpicImpl cacheServiceEpicImpl;
+
+	private final CacheServiceSprintImpl cacheServiceSprintImpl;
+
+	private final CacheService cacheService;
+
+	private final CacheServiceProjectAddOn cacheServiceProjectAddOn;
+
 	@DeleteMapping("/project/{id}")
 	@Transactional
 	public ResponseEntity<Void> deleteProject(@PathVariable long id) {
-
-		projectRepo.deleteById(id);
 		
+		Project project = projectRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("entity with requested Id not Found"));
+		
+		cacheServiceProjectAddOn.DeleteEntitiesInCacheOnProjectDeletion(project);
+		
+		projectRepo.deleteById(id);
+
 		return ResponseEntity.status(204).build();
 	}
 
@@ -66,8 +95,13 @@ public class DeleteEntitiesAdminController {
 	@Transactional
 	public ResponseEntity<Void> deleteEpic(@PathVariable long id) {
 
+		Epic epic = epicRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("entity with requested Id not Found"));
+
+		cacheServiceEpicImpl.deleteEpicFromCacheAndCascadeDeleteChildren(epic);
+
 		epicRepo.deleteById(id);
-		
+
 		return ResponseEntity.status(204).build();
 	}
 
@@ -75,8 +109,13 @@ public class DeleteEntitiesAdminController {
 	@Transactional
 	public ResponseEntity<Void> deleteSprint(@PathVariable long id) {
 
-		sprintRepo.deleteById(id);
+		Sprint sprint = sprintRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("entity with requested Id not Found"));
+
+		cacheServiceSprintImpl.deleteSprintFromCacheAndCascadeDeleteChildren(sprint);
 		
+		sprintRepo.deleteById(id);
+
 		return ResponseEntity.status(204).build();
 	}
 
@@ -84,8 +123,15 @@ public class DeleteEntitiesAdminController {
 	@Transactional
 	public ResponseEntity<Void> deleteUserStory(@PathVariable long id) {
 
-		userStoryRepo.deleteById(id);
+		UserStory userStory = userStoryRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("entity with requested Id not Found"));
 		
+		cacheService.deleteObjectFromCache(CacheServiceUserStoryImpl.epicUserStoryListCache, userStory.getEpic().getId(), userStory, UserStory::getId);
+		
+		cacheService.deleteObjectFromCache(CacheServiceUserStoryImpl.sprintUserStoryListCache, userStory.getSprint().getId(), userStory, UserStory::getId);
+		
+		userStoryRepo.deleteById(id);
+
 		return ResponseEntity.status(204).build();
 	}
 
@@ -94,7 +140,7 @@ public class DeleteEntitiesAdminController {
 	public ResponseEntity<Void> deleteTask(@PathVariable long id) {
 
 		taskRepo.deleteById(id);
-		
+
 		return ResponseEntity.status(204).build();
 	}
 
@@ -103,7 +149,7 @@ public class DeleteEntitiesAdminController {
 	public ResponseEntity<Void> deleteTaskAttachment(@PathVariable long id) {
 
 		taskAttachmentRepo.deleteById(id);
-		
+
 		return ResponseEntity.status(204).build();
 	}
 
@@ -112,7 +158,7 @@ public class DeleteEntitiesAdminController {
 	public ResponseEntity<Void> deleteAnnouncement(@PathVariable long id) {
 
 		announcementRepo.deleteById(id);
-		
+
 		return ResponseEntity.status(204).build();
 	}
 
@@ -121,7 +167,7 @@ public class DeleteEntitiesAdminController {
 	public ResponseEntity<Void> deleteInvitation(@PathVariable long id) {
 
 		invitationRepo.deleteById(id);
-		
+
 		return ResponseEntity.status(204).build();
 	}
 
