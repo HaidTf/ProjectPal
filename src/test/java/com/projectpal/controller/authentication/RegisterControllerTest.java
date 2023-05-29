79b5efd1b13f541f099dd7b9b8831d79bc409c7d
haidtf <haidartfeily25@gmail.com>
2023-05-29 14:27:37 +0300

    changed expected response status in testRegisterWithNoData method to 400

--- src/test/java/com/projectpal/controller/authentication/RegisterControllerTest.java
+++ src/test/java/com/projectpal/controller/authentication/RegisterControllerTest.java
@@ -55,7 +55,7 @@ public class RegisterControllerTest {
 	@Test
 	public void testRegisterWithNoData() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.post("/auth/register").contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isInternalServerError());
+				.accept(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isBadRequest());
 
 	}
 
