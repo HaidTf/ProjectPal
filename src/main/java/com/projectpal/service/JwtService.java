70cd43d6dad0e161654e075a654adf5ae530f112
haidtf <haidartfeily25@gmail.com>
2023-05-31 13:41:14 +0300

    modified jwt expiration date to 1 week in generateToken method in JwtService

--- src/main/java/com/projectpal/service/JwtService.java
+++ src/main/java/com/projectpal/service/JwtService.java
@@ -29,7 +29,7 @@ public class JwtService {
 	public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
 		return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
 				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24 * 7))
+				.setExpiration(new Date(System.currentTimeMillis() + 6 * 100000000))
 				.signWith(getSignInKey(), SignatureAlgorithm.HS256).compact();
 	}
 
