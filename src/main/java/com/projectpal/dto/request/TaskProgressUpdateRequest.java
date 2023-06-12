92be30c56b4267a9f92a1bc62bc81f07c6856ec0
haidtf <haidartfeily25@gmail.com>
2023-06-12 21:09:59 +0300

    created TaskProgressUpdateRequest class in dto/request package

---
+++ src/main/java/com/projectpal/dto/request/TaskProgressUpdateRequest.java
@@ -0,0 +1,34 @@
+package com.projectpal.dto.request;
+
+
+
+import org.springframework.lang.Nullable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.projectpal.entity.enums.Progress;
+
+import jakarta.validation.constraints.NotNull;
+
+public class TaskProgressUpdateRequest {
+
+	public TaskProgressUpdateRequest(Progress progress, String report) {
+		this.progress = progress;
+		this.report = report;
+	}
+
+	@NotNull
+	private final Progress progress;
+
+	@Nullable
+	@JsonProperty("report")
+	private final String report;
+
+	public Progress getProgress() {
+		return progress;
+	}
+
+	public String getReport() {
+		return report;
+	}
+
+}
