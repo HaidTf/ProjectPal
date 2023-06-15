c49964f5031aea3417783f90e0022e471c9b5add
haidtf <haidartfeily25@gmail.com>
2023-06-16 00:45:06 +0300

    implemented findAllByProjectIdAndProgressNot in SprintRepository

--- src/main/java/com/projectpal/repository/SprintRepository.java
+++ src/main/java/com/projectpal/repository/SprintRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import com.projectpal.entity.Project;
 import com.projectpal.entity.Sprint;
+import com.projectpal.entity.enums.Progress;
 @Repository
 public interface SprintRepository extends JpaRepository<Sprint,Long>{
 
@@ -16,5 +17,7 @@ public interface SprintRepository extends JpaRepository<Sprint,Long>{
 	Optional<List<Sprint>> findAllByProjectId(Long id);
 	
 	int countByProjectId(Long projectId);
+
+	Optional<List<Sprint>> findAllByProjectIdAndProgressNot(long id, Progress progress);
 	
 }
