10c819406653e5c2d5ad80360eafc0c80f52984c
haidtf <haidartfeily25@gmail.com>
2023-06-19 19:29:27 +0300

    created DataSourceConfig class for datasource config based on profiles

---
+++ src/main/java/com/projectpal/configuration/DataSourceConfig.java
@@ -0,0 +1,36 @@
+package com.projectpal.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import jakarta.activation.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+	@Bean
+	@Profile("development")
+	HikariDataSource developmentDataSource() {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/projectpal");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		return dataSource;
+	}
+
+	@Bean
+	@Profile("production")
+	HikariDataSource productionDataSource() {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/projectpal");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		return dataSource;
+	}
+
+}
