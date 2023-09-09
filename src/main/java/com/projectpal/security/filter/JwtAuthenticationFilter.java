13340dbafeaf719202a0531dce6129a31acaed00
haidtf <haidartfeily25@gmail.com>
2023-09-09 21:26:40 +0300

    JwtAuthenticationFilter: implemented better exception handling and modified flow to call CustomAuthenticationEntryPoint.commence() in case of authentication exception

--- src/main/java/com/projectpal/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/projectpal/security/filter/JwtAuthenticationFilter.java
@@ -3,18 +3,21 @@ package com.projectpal.security.filter;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.projectpal.exception.CustomAuthenticationException;
+import com.projectpal.security.CustomAuthenticationEntryPoint;
 import com.projectpal.service.JwtService;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,14 +27,18 @@ import jakarta.servlet.http.HttpServletResponse;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	@Autowired
-	public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+	public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService,
+			CustomAuthenticationEntryPoint authEntryPoint) {
 		this.jwtService = jwtService;
 		this.userDetailsService = userDetailsService;
+		this.authEntryPoint = authEntryPoint;
 	}
 
 	private final JwtService jwtService;
 
 	private final UserDetailsService userDetailsService;
+
+	private final CustomAuthenticationEntryPoint authEntryPoint;
 
 	@Override
 	protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
@@ -43,20 +50,45 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
 
-			filterChain.doFilter(request, response);
+			authEntryPoint.commence(request, response, new CustomAuthenticationException(
+					"Jwt Authentication failed: No Jwt token is in the Authorization header"));
 			return;
 		}
-		jwt = authHeader.substring(7);
-		userEmail = jwtService.extractEmail(jwt);
 
+		try {
+
+			jwt = authHeader.substring(7);
+
+			userEmail = jwtService.extractEmail(jwt);
+
+		} catch (IndexOutOfBoundsException ex) {
+			authEntryPoint.commence(request, response, new CustomAuthenticationException(
+					"Jwt Authentication failed: No Jwt token is in the Authorization header", ex));
+			return;
+			
+		} catch (ExpiredJwtException ex) {
+			authEntryPoint.commence(request, response,
+					new CustomAuthenticationException("Jwt Authentication failed: Jwt token is expired", ex));
+			return;
+
+		}
+		
 		if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-			UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-			
-			if (userDetails == null) {
-				response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+			UserDetails userDetails;
+
+			try {
+
+				userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
+			} catch (UsernameNotFoundException ex) {
+
+				authEntryPoint.commence(request, response,
+						new CustomAuthenticationException("Jwt Authentication failed: User not found", ex));
 				return;
+
 			}
-			
+
 			if (jwtService.isTokenValid(jwt, userDetails)) {
 
 				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
