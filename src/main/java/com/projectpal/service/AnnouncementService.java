d07a23e770968954d11f0a52d727e562b3a41513
haidtf <haidartfeily25@gmail.com>
2023-10-02 11:11:53 +0300

    feat(AnnouncementService): create new class for Announcement related service layer operations
    -findAnnouncementById
    -createAnnouncement
    -deleteAnnouncement
    -findPageByProject

---
+++ src/main/java/com/projectpal/service/AnnouncementService.java
@@ -0,0 +1,52 @@
+package com.projectpal.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.projectpal.entity.Announcement;
+import com.projectpal.entity.Project;
+import com.projectpal.exception.ResourceNotFoundException;
+import com.projectpal.repository.AnnouncementRepository;
+import com.projectpal.utils.ProjectUtil;
+
+@Service
+public class AnnouncementService {
+
+	@Autowired
+	public AnnouncementService(AnnouncementRepository announcementRepo) {
+		this.announcementRepo = announcementRepo;
+	}
+
+	private final AnnouncementRepository announcementRepo;
+
+	public Announcement findAnnouncementById(long announcementId) {
+		return announcementRepo.findById(announcementId)
+				.orElseThrow(() -> new ResourceNotFoundException("Announcement does not exist"));
+
+	}
+
+	public void createAnnouncement(Project project, Announcement announcement) {
+
+		announcement.setProject(ProjectUtil.getProjectNotNull());
+
+		announcementRepo.save(announcement);
+
+	}
+
+	public void deleteAnnouncement(Announcement announcement) {
+
+		announcementRepo.delete(announcement);
+	}
+
+	public Page<Announcement> findPageByProject(Project project, int page, int size) {
+
+		Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Order.desc("issueDate")));
+
+		return announcementRepo.findAllByProject(project, pageable);
+	}
+
+}
