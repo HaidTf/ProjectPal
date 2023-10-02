845bcf3dcd608a694c8b5c06b874441dfce381ac
haidtf <haidartfeily25@gmail.com>
2023-10-02 11:20:19 +0300

    feat(SprintCacheService): create new class for Sprint related cache operations

---
+++ src/main/java/com/projectpal/service/SprintCacheService.java
@@ -0,0 +1,17 @@
+package com.projectpal.service;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.stereotype.Service;
+
+import com.projectpal.entity.Sprint;
+
+@Service
+@Qualifier("sprintCacheService")
+public class SprintCacheService extends CacheService<Sprint> {
+
+	public SprintCacheService(RedisCacheManager redis) {
+		super(redis);
+	}
+
+}
