ce53a3abf7f605d8b7c2e0eeae59a4b346346ac8
haidtf <haidartfeily25@gmail.com>
2023-10-08 19:35:59 +0300

    refactor(ProjectController): move remaining business logic into services and replace use cases of SecurityContextUtil with @AuthenticationPrincipal method parameter annotation

--- src/main/java/com/projectpal/controller/ProjectController.java
+++ src/main/java/com/projectpal/controller/ProjectController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,11 +25,9 @@ import com.projectpal.dto.response.CustomPageResponse;
 import com.projectpal.entity.Project;
 import com.projectpal.entity.User;
 import com.projectpal.entity.enums.Role;
-import com.projectpal.exception.BadRequestException;
-import com.projectpal.exception.ForbiddenException;
+import com.projectpal.exception.ResourceNotFoundException;
 import com.projectpal.service.ProjectService;
 import com.projectpal.service.UserService;
-import com.projectpal.utils.SecurityContextUtil;
 
 import jakarta.validation.Valid;
 
@@ -47,9 +46,10 @@ public class ProjectController {
 	private final UserService userService;
 
 	@GetMapping("")
-	public ResponseEntity<Project> getProject() {
+	public ResponseEntity<Project> getProject(@AuthenticationPrincipal User currentUser) {
 
-		Project project = SecurityContextUtil.getUserProject();
+		Project project = currentUser.getOptionalOfProject()
+				.orElseThrow(() -> new ResourceNotFoundException("User is not in a project"));
 
 		projectService.updateProjectLastAccessedDate(project);
 
@@ -57,11 +57,12 @@ public class ProjectController {
 	}
 
 	@GetMapping("/users")
-	public ResponseEntity<CustomPageResponse<User>> getProjectMembers(@RequestParam(required = false) Role role,
-			@RequestParam(required = false, defaultValue = "0") int page,
+	public ResponseEntity<CustomPageResponse<User>> getProjectMembers(@AuthenticationPrincipal User currentUser,
+			@RequestParam(required = false) Role role, @RequestParam(required = false, defaultValue = "0") int page,
 			@RequestParam(required = false, defaultValue = "20") int size) {
 
-		Project project = SecurityContextUtil.getUserProjectNotNull();
+		Project project = currentUser.getOptionalOfProject()
+				.orElseThrow(() -> new ResourceNotFoundException("User is not in a project"));
 
 		Page<User> users = userService.findAllByProjectAndRole(project, role, page, size);
 
@@ -71,11 +72,10 @@ public class ProjectController {
 
 	@PreAuthorize("hasAnyRole('USER','USER_PROJECT_OPERATOR','USER_PROJECT_PARTICIPATOR')")
 	@PostMapping("")
-	public ResponseEntity<Project> createProject(@Valid @RequestBody Project project) {
+	public ResponseEntity<Project> createProject(@AuthenticationPrincipal User currentUser,
+			@Valid @RequestBody Project project) {
 
-		User user = SecurityContextUtil.getUser();
-
-		projectService.createProjectAndSetOwner(project, user);
+		projectService.createProjectAndSetOwner(project, currentUser);
 
 		UriComponents uriComponents = UriComponentsBuilder.fromPath("/api/project").build();
 		URI location = uriComponents.toUri();
@@ -85,9 +85,10 @@ public class ProjectController {
 
 	@PreAuthorize("hasAnyRole('USER_PROJECT_OWNER','USER_PROJECT_OPERATOR')")
 	@PatchMapping("/description")
-	public ResponseEntity<Void> updateDescription(@RequestBody DescriptionUpdateRequest descriptionUpdateRequest) {
+	public ResponseEntity<Void> updateDescription(@AuthenticationPrincipal User currentUser,
+			@RequestBody DescriptionUpdateRequest descriptionUpdateRequest) {
 
-		Project project = SecurityContextUtil.getUserProject();
+		Project project = currentUser.getProject();
 
 		projectService.updateProjectDescription(project, descriptionUpdateRequest.getDescription());
 
@@ -97,22 +98,10 @@ public class ProjectController {
 
 	@PreAuthorize("hasAnyRole('USER_PROJECT_OWNER','ADMIN')")
 	@PatchMapping("/users/{userId}/role")
-	public ResponseEntity<Void> setUserProjectRole(@PathVariable long userId,
-			@RequestBody @Valid RoleUpdateRequest roleUpdateRequest) {
+	public ResponseEntity<Void> setOtherUserProjectRole(@AuthenticationPrincipal User currentUser,
+			@PathVariable long otherUserId, @RequestBody @Valid RoleUpdateRequest roleUpdateRequest) {
 
-		User user = userService.findUserById(userId);
-
-		if (user.getProject().getId() != SecurityContextUtil.getUserProject().getId())
-			throw new ForbiddenException("the user must be in the project");
-
-		if (SecurityContextUtil.getUser().getId() == userId)
-			throw new BadRequestException("you can not change your own role");
-
-		if (roleUpdateRequest.getRole() != Role.ROLE_USER_PROJECT_PARTICIPATOR
-				|| roleUpdateRequest.getRole() != Role.ROLE_USER_PROJECT_OPERATOR)
-			throw new BadRequestException("You are not allowed to set this role");
-
-		userService.updateUserRole(user, roleUpdateRequest.getRole());
+		userService.updateUserProjectRole(currentUser, otherUserId, roleUpdateRequest.getRole());
 
 		return ResponseEntity.status(204).build();
 
@@ -120,26 +109,19 @@ public class ProjectController {
 
 	@PreAuthorize("hasAnyRole('USER_PROJECT_OWNER','USER_PROJECT_OPERATOR')")
 	@DeleteMapping("/users/{userId}/membership")
-	public ResponseEntity<Void> removeUserFromProject(@PathVariable long userId) {
+	public ResponseEntity<Void> removeOtherUserFromProject(@AuthenticationPrincipal User currentUser,
+			@PathVariable long otherUserId) {
 
-		User user = userService.findUserById(userId);
-
-		if (user.getProject().getId() != SecurityContextUtil.getUserProject().getId())
-			throw new ForbiddenException("the user must be in the project");
-
-		if (SecurityContextUtil.getUser().getId() == userId)
-			throw new BadRequestException("you cant remove yourself from the project through here");
-
-		userService.exitUserProject(user);
+		projectService.removeUserFromCurrentUserProject(currentUser, otherUserId);
 
 		return ResponseEntity.status(204).build();
 	}
 
 	@PreAuthorize("hasAnyRole('USER_PROJECT_OWNER','ADMIN')")
 	@DeleteMapping("")
-	public ResponseEntity<Void> deleteProject() {
+	public ResponseEntity<Void> deleteProject(@AuthenticationPrincipal User currentUser) {
 
-		Project project = SecurityContextUtil.getUserProject();
+		Project project = currentUser.getProject();
 
 		projectService.deleteProject(project);
 
