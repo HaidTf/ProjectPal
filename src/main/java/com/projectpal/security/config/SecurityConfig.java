b63339075af428e5f71eb8da833f799b72890a35
haidtf <haidartfeily25@gmail.com>
2023-10-08 18:25:34 +0300

    refactor(SecurityConfig,UserDetailsServiceConfig): moved classes from com/projectpal/config to com/projectpal/security/config

--- src/main/java/com/projectpal/security/config/SecurityConfig.java
+++ src/main/java/com/projectpal/security/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.projectpal.configuration;
+package com.projectpal.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -21,7 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.projectpal.security.CustomAuthenticationEntryPoint;
+import com.projectpal.security.entrypoint.CustomAuthenticationEntryPoint;
 import com.projectpal.security.filter.JwtAuthenticationFilter;
 
 @Configuration
--- src/main/java/com/projectpal/security/config/UserDetailsServiceConfig.java
+++ src/main/java/com/projectpal/security/config/UserDetailsServiceConfig.java
@@ -1,4 +1,4 @@
-package com.projectpal.configuration;
+package com.projectpal.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
