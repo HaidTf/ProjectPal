20164856d8bd57ee27a4ca1a3b91666ee76de4e6
haidtf <haidartfeily25@gmail.com>
2023-10-09 12:35:15 +0300

    refactor(AnnouncementController): move remaining business logic into AnnouncementService and replace use cases of SecurityContextUtil with @AuthenticationPrincipal method parameter annotation

--- src/main/java/com/projectpal/controller/AnnouncementController.java
+++ src/main/java/com/projectpal/controller/AnnouncementController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,10 +20,10 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.projectpal.dto.response.CustomPageResponse;
 import com.projectpal.entity.Announcement;
-import com.projectpal.entity.Project;
-import com.projectpal.exception.ForbiddenException;
+import com.projectpal.entity.User;
 import com.projectpal.service.AnnouncementService;
-import com.projectpal.utils.SecurityContextUtil;
+import com.projectpal.utils.ProjectMembershipValidationUtil;
+import com.projectpal.utils.UserEntityAccessValidationUtil;
 
 import jakarta.validation.Valid;
 
@@ -38,38 +39,37 @@ public class AnnouncementController {
 	private final AnnouncementService announcementService;
 
 	@GetMapping("/{announcementId}")
-	public ResponseEntity<Announcement> getAnnouncement(@PathVariable long announcementId) {
+	public ResponseEntity<Announcement> getAnnouncement(@AuthenticationPrincipal User currentUser,
+			@PathVariable long announcementId) {
 
-		Project project = SecurityContextUtil.getUserProjectNotNull();
+		ProjectMembershipValidationUtil.verifyUserProjectMembership(currentUser);
 
 		Announcement announcement = announcementService.findAnnouncementById(announcementId);
 
-		if (announcement.getProject().getId() != project.getId())
-			throw new ForbiddenException("You are not allowed access to other projects");
+		UserEntityAccessValidationUtil.verifyUserAccessToAnnouncement(currentUser, announcement);
 
 		return ResponseEntity.ok(announcement);
 
 	}
 
 	@GetMapping("")
-	public ResponseEntity<CustomPageResponse<Announcement>> getAnnouncements(
+	public ResponseEntity<CustomPageResponse<Announcement>> getAnnouncements(@AuthenticationPrincipal User currentUser,
 			@RequestParam(required = false, defaultValue = "0") int page,
 			@RequestParam(required = false, defaultValue = "5") int size) {
 
-		Project project = SecurityContextUtil.getUserProjectNotNull();
+		ProjectMembershipValidationUtil.verifyUserProjectMembership(currentUser);
 
-		Page<Announcement> announcements = announcementService.findPageByProject(project, page, size);
+		Page<Announcement> announcements = announcementService.findPageByProject(currentUser.getProject(), page, size);
 
 		return ResponseEntity.ok(new CustomPageResponse<Announcement>(announcements));
 	}
 
 	@PreAuthorize("hasAnyRole('USER_PROJECT_OWNER','USER_PROJECT_OPERATOR')")
 	@PostMapping("")
-	public ResponseEntity<Announcement> createAnnouncement(@Valid @RequestBody Announcement announcement) {
+	public ResponseEntity<Announcement> createAnnouncement(@AuthenticationPrincipal User currentUser,
+			@Valid @RequestBody Announcement announcement) {
 
-		Project project = SecurityContextUtil.getUserProject();
-
-		announcementService.createAnnouncement(project, announcement);
+		announcementService.createAnnouncement(currentUser.getProject(), announcement);
 
 		UriComponents uriComponents = UriComponentsBuilder
 				.fromPath("/api/project/announcements/" + announcement.getId()).build();
@@ -83,12 +83,7 @@ public class AnnouncementController {
 	@DeleteMapping("/{AnnouncementId}")
 	public ResponseEntity<Void> deleteAnnouncement(@PathVariable long announcementId) {
 
-		Announcement announcement = announcementService.findAnnouncementById(announcementId);
-
-		if (announcement.getProject().getId() != SecurityContextUtil.getUserProject().getId())
-			throw new ForbiddenException("you are not allowed to access other projects");
-
-		announcementService.deleteAnnouncement(announcement);
+		announcementService.deleteAnnouncement(announcementId);
 
 		return ResponseEntity.status(204).build();
 	}
