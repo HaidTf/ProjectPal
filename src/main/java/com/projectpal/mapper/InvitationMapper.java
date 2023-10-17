e3c9b26a976ffde4db21a05a01accd1ab2a7c766
haidtf <haidartfeily25@gmail.com>
2023-10-17 17:21:29 +0300

    feat(InvitationMapper): add mapping method for ProjectInvitationResponseDto

--- src/main/java/com/projectpal/mapper/InvitationMapper.java
+++ src/main/java/com/projectpal/mapper/InvitationMapper.java
@@ -3,7 +3,8 @@ package com.projectpal.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import com.projectpal.dto.response.entity.InvitationResponseDto;
+import com.projectpal.dto.response.entity.ProjectInvitationResponseDto;
+import com.projectpal.dto.response.entity.UserInvitationResponseDto;
 import com.projectpal.entity.Invitation;
 
 @Mapper(componentModel = "spring")
@@ -12,6 +13,10 @@ public interface InvitationMapper {
 	@Mapping(source = "invitedUser.id", target = "invitedUserId")
 	@Mapping(source = "project.id", target = "projectId")
 	@Mapping(source = "project.name", target = "projectName")
-	InvitationResponseDto toDto(Invitation invitation);
+	UserInvitationResponseDto toUserInvitationDto(Invitation invitation);
 
+	@Mapping(source = "invitedUser.id", target = "invitedUserId")
+	@Mapping(source = "invitedUser.name", target = "invitedUserId")
+	ProjectInvitationResponseDto toProjectInvitationDto(Invitation invitation);
+	
 }
