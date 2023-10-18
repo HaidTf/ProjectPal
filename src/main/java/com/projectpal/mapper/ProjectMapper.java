e74a456a9c389550e0e9a69f86c93d37f558ea19
haidtf <haidartfeily25@gmail.com>
2023-10-18 17:33:37 +0300

    refactor(AnnouncementMapper,ProjectMapper):remove toDto() method

--- src/main/java/com/projectpal/mapper/ProjectMapper.java
+++ src/main/java/com/projectpal/mapper/ProjectMapper.java
@@ -4,13 +4,10 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import com.projectpal.dto.request.entity.ProjectCreationDto;
-import com.projectpal.dto.response.entity.ProjectResponseDto;
 import com.projectpal.entity.Project;
 
 @Mapper(componentModel = "spring")
 public interface ProjectMapper {
-
-	ProjectResponseDto toDto(Project project);
 
 	@Mapping(source = "nameAndDescriptionAttribute.name", target = "name")
 	@Mapping(source = "nameAndDescriptionAttribute.description", target = "description")
