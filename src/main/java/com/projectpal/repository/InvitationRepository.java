95ab9a72ba94ce163dec571ec9d15911b5a2e63d
haidtf <haidartfeily25@gmail.com>
2023-10-18 16:54:15 +0300

    feature(InvitationRepository):  add two methods findSentInvitationById, findReceivedInvitationById and use entity graphs for these methods and other methods that fetch received or sent invitations

--- src/main/java/com/projectpal/repository/InvitationRepository.java
+++ src/main/java/com/projectpal/repository/InvitationRepository.java
@@ -2,10 +2,12 @@ package com.projectpal.repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +16,7 @@ import com.projectpal.entity.Project;
 import com.projectpal.entity.User;
 
 @Repository
-public interface InvitationRepository extends JpaRepository<Invitation,Long> {
+public interface InvitationRepository extends JpaRepository<Invitation, Long> {
 
 	List<Invitation> findAllByInvitedUser(User invitedUser);
 
@@ -22,8 +24,16 @@ public interface InvitationRepository extends JpaRepository<Invitation,Long> {
 
 	void deleteByIssueDateBefore(LocalDate xDateAgo);
 
+	@EntityGraph(attributePaths = { "invitedUser.id", "invitedUser.name" })
 	Page<Invitation> findAllByProject(Project project, Pageable pageable);
 
+	@EntityGraph(attributePaths = { "project.id", "project.name" })
 	List<Invitation> findAllByInvitedUser(User user, Sort sort);
-	
+
+	@EntityGraph(attributePaths = { "invitedUser.id", "invitedUser.name" })
+	Optional<Invitation> findSentInvitationById(long invitationId);
+
+	@EntityGraph(attributePaths = { "project.id", "project.name" })
+	Optional<Invitation> findReceivedInvitationById(long invitationId);
+
 }
