ad7b798e8351acc4913c5b54be5112245ac9c41a
haidtf <haidartfeily25@gmail.com>
2023-10-21 20:22:01 +0300

    feat(AnnouncementRepository): create new method findAnnouncementDtoPageByProject()

--- src/main/java/com/projectpal/repository/AnnouncementRepository.java
+++ src/main/java/com/projectpal/repository/AnnouncementRepository.java
@@ -6,18 +6,25 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.projectpal.dto.response.entity.AnnouncementResponseDto;
 import com.projectpal.entity.Announcement;
 import com.projectpal.entity.Project;
 
 @Repository
-public interface AnnouncementRepository extends JpaRepository<Announcement,Long>{
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
 
 	List<Announcement> findAllByProject(Project project);
 
 	void deleteByIssueDateBefore(LocalDate xDateAgo);
 
 	Page<Announcement> findAllByProject(Project project, Pageable pageable);
+
+	@Query("SELECT new com.projectpal.dto.response.entity.AnnouncementResponseDto(a.id,a.title,a.description,a.issueDate,u.id,u.name) FROM Announcement a JOIN a.announcer u WHERE a.project = :project")
+	Page<AnnouncementResponseDto> findAnnouncementDtoPageByProject(@Param("project") Project project,
+			Pageable pageable);
 
 }
