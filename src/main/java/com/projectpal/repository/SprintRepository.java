76e070126cfc47ac47bd2785f901a10db9555338
haidtf <haidartfeily25@gmail.com>
2023-10-24 19:39:20 +0300

    feat(SprintRepository): add method findByIdAndProject

--- src/main/java/com/projectpal/repository/SprintRepository.java
+++ src/main/java/com/projectpal/repository/SprintRepository.java
@@ -1,6 +1,7 @@
 package com.projectpal.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.data.domain.Sort;
@@ -10,13 +11,16 @@ import org.springframework.stereotype.Repository;
 import com.projectpal.entity.Project;
 import com.projectpal.entity.Sprint;
 import com.projectpal.entity.enums.Progress;
+
 @Repository
-public interface SprintRepository extends JpaRepository<Sprint,Long>{
+public interface SprintRepository extends JpaRepository<Sprint, Long> {
+
+	Optional<Sprint> findByIdAndProject(long sprintId, Project project);
 
 	List<Sprint> findAllByProject(Project project);
 
 	List<Sprint> findAllByProjectId(Long id);
-	
+
 	int countByProjectId(Long projectId);
 
 	List<Sprint> findAllByProjectIdAndProgressNot(long id, Progress progress);
@@ -26,5 +30,5 @@ public interface SprintRepository extends JpaRepository<Sprint,Long>{
 	List<Sprint> findAllByProject(Project project, Sort sort);
 
 	List<Sprint> findAllByProjectAndProgressIn(Project project, Set<Progress> progress);
-	
+
 }
