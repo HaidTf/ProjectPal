ecf1c27cfe2bedb59e0a04f64e019c6866d68492
haidtf <haidartfeily25@gmail.com>
2023-10-24 19:40:12 +0300

    feat(UserStoryRepository): add method findByIdAndEpicProject

--- src/main/java/com/projectpal/repository/UserStoryRepository.java
+++ src/main/java/com/projectpal/repository/UserStoryRepository.java
@@ -1,6 +1,7 @@
 package com.projectpal.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.data.domain.Sort;
@@ -8,22 +9,26 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projectpal.entity.Epic;
+import com.projectpal.entity.Project;
 import com.projectpal.entity.Sprint;
 import com.projectpal.entity.UserStory;
 import com.projectpal.entity.enums.Progress;
+
 @Repository
-public interface UserStoryRepository extends JpaRepository<UserStory,Long>{
+public interface UserStoryRepository extends JpaRepository<UserStory, Long> {
+
+	Optional<UserStory> findByIdAndEpicProject(long id, Project project);
 
 	List<UserStory> findAllByEpic(Epic epic);
-	
+
 	List<UserStory> findAllBySprint(Sprint sprint);
 
 	List<UserStory> findAllByEpicId(Long id);
-	
+
 	List<UserStory> findAllBySprintId(Long id);
-	
+
 	int countByEpicId(Long projectId);
-	
+
 	int countBySprintId(Long sprintId);
 
 	List<UserStory> findAllByEpicAndProgressIn(Epic epic, Set<Progress> progress);
@@ -37,5 +42,5 @@ public interface UserStoryRepository extends JpaRepository<UserStory,Long>{
 	List<UserStory> findAllBySprintAndProgressIn(Sprint sprint, Set<Progress> progress, Sort sort);
 
 	List<UserStory> findAllBySprintAndProgressIn(Sprint sprint, Set<Progress> progress);
-	
+
 }
