65fac24b2646375cddd4b279454dc72e11710387
haidtf <haidartfeily25@gmail.com>
2023-10-25 11:25:09 +0300

    refactor(ProjectService): remove EpicService and SprintService from dependancies and use EpicRepository and SprintRepository instead

--- src/main/java/com/projectpal/service/ProjectService.java
+++ src/main/java/com/projectpal/service/ProjectService.java
@@ -1,9 +1,7 @@
 package com.projectpal.service;
 
 import java.util.List;
-import java.util.Set;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
@@ -19,7 +17,9 @@ import com.projectpal.entity.enums.Role;
 import com.projectpal.exception.BadRequestException;
 import com.projectpal.exception.ForbiddenException;
 import com.projectpal.exception.ResourceNotFoundException;
+import com.projectpal.repository.EpicRepository;
 import com.projectpal.repository.ProjectRepository;
+import com.projectpal.repository.SprintRepository;
 import com.projectpal.repository.UserRepository;
 import com.projectpal.service.cache.CacheService;
 
@@ -33,9 +33,9 @@ public class ProjectService {
 
 	private final UserRepository userRepo;
 
-	private final EpicService epicService;
+	private final EpicRepository epicRepo;
 
-	private final SprintService sprintService;
+	private final SprintRepository sprintRepo;
 
 	private final TaskService taskService;
 
@@ -44,8 +44,7 @@ public class ProjectService {
 	@Transactional(readOnly = true)
 	public ProjectResponseDto findProjectDtoById(long id) {
 
-		return projectRepo.findProjectDtoById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Project not found"));
+		return projectRepo.findProjectDtoById(id).orElseThrow(() -> new ResourceNotFoundException("Project not found"));
 	}
 
 	@Transactional
@@ -111,8 +110,8 @@ public class ProjectService {
 		cacheService.evictListFromCache(Epic.EPIC_CACHE, project.getId());
 		cacheService.evictListFromCache(Sprint.SPRINT_CACHE, project.getId());
 
-		List<Epic> epics = epicService.findEpicsByProjectAndProgressFromDb(project, Set.of(), Sort.unsorted());
-		List<Sprint> sprints = sprintService.findSprintsByProjectAndProgressFromDb(project, Set.of(), Sort.unsorted());
+		List<Epic> epics = epicRepo.findAllByProject(project);
+		List<Sprint> sprints = sprintRepo.findAllByProject(project);
 
 		epics.forEach((epic) -> cacheService.evictListFromCache(UserStory.EPIC_USERSTORY_CACHE, epic.getId()));
 		sprints.forEach((sprint) -> cacheService.evictListFromCache(UserStory.SPRINT_USERSTORY_CACHE, sprint.getId()));
