f1f9c8327acf8e3bc5c38fcc6c665536305b36ed
haidtf <haidartfeily25@gmail.com>
2023-10-25 16:37:02 +0300

    feat: create CacheConstants class for storing cache related constants

---
+++ src/main/java/com/projectpal/service/cache/CacheConstants.java
@@ -0,0 +1,12 @@
+package com.projectpal.service.cache;
+
+public class CacheConstants {
+
+	public static final String EPIC_CACHE = "epicListCache";
+	
+	public static final String SPRINT_CACHE = "sprintListCache";
+	
+	public static final String EPIC_USERSTORY_CACHE = "epicUserStoryListCache";
+
+	public static final String SPRINT_USERSTORY_CACHE = "sprintUserStoryListCache";
+}
