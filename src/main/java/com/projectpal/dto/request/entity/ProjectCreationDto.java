216036948feb4cdd00e38ede58f75b42c10c8e63
haidtf <haidartfeily25@gmail.com>
2023-10-25 13:55:38 +0300

    refactor(entity-creation-dtos): update entity creation dtos to explicitly instantiate nested objects through the json creator constructor that takes all required properties

--- src/main/java/com/projectpal/dto/request/entity/ProjectCreationDto.java
+++ src/main/java/com/projectpal/dto/request/entity/ProjectCreationDto.java
@@ -1,17 +1,19 @@
 package com.projectpal.dto.request.entity;
 
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.projectpal.dto.request.NameAndDescriptionDto;
 
 import jakarta.validation.Valid;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class ProjectCreationDto {
 
-	@JsonUnwrapped
+	@JsonCreator
+	public ProjectCreationDto(String name,String description) {
+		this.nameAndDescriptionAttribute = new NameAndDescriptionDto(name,description);
+	}
+	
 	@Valid
 	private final NameAndDescriptionDto nameAndDescriptionAttribute;
 
