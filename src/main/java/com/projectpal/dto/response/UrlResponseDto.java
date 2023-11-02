0155596ea627969f854c7bb7067e0a38306cbb89
haidtf <haidartfeily25@gmail.com>
2023-11-02 19:48:00 +0200

    feat: create new Dto UrlResponseDto

---
+++ src/main/java/com/projectpal/dto/response/UrlResponseDto.java
@@ -0,0 +1,12 @@
+package com.projectpal.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class UrlResponseDto {
+
+	private final String Url;
+
+}
