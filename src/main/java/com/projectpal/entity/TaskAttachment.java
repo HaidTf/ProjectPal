54e46fa0d58bde834d30680ace1d41b1388313db
haidtf <haidartfeily25@gmail.com>
2023-11-08 16:14:46 +0200

    refactor(TaskAttachment): set column definitions for fileName and mimeType fields, set nullable = false for fileName, fileSize, mimeType and creationDate fields

--- src/main/java/com/projectpal/entity/TaskAttachment.java
+++ src/main/java/com/projectpal/entity/TaskAttachment.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.CreatedDate;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -40,15 +41,19 @@ public class TaskAttachment implements Serializable {
 	private long id;
 
 	@NotBlank
+	@Column(columnDefinition = "VARCHAR(500)", nullable = false)
 	private String fileName;
 
+	@Column(nullable = false)
 	private long fileSize;
 
+	@Column(columnDefinition = "VARCHAR(100)", nullable = false)
 	private String mimeType;
 
 	@Temporal(TemporalType.DATE)
 	@CreatedDate
 	@Setter(AccessLevel.NONE)
+	@Column(nullable = false)
 	private LocalDate creationDate;
 
 	@ManyToOne
