588eee3d291e942ee0f9eaa0ed519b4d20417b4c
haidtf <haidartfeily25@gmail.com>
2023-11-10 21:20:13 +0200

    refactor(CustomErrorController):add logging functionality using Slf4j

--- src/main/java/com/projectpal/controller/CustomErrorController.java
+++ src/main/java/com/projectpal/controller/CustomErrorController.java
@@ -9,23 +9,28 @@ import com.projectpal.exception.server.InternalServerErrorException;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class CustomErrorController implements ErrorController {
 
 	@RequestMapping("/error")
 	public void handleError(HttpServletRequest request) {
 
-		System.out.println("custom error controller called");
-		
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
 		if (status != null) {
 			Integer statusCode = Integer.valueOf(status.toString());
 
+			log.debug("CustomErrorController invoked with status {}", statusCode);
+
 			if (statusCode == 404)
 				throw new ResourceNotFoundException("Endpoint not found");
 		}
+		
+		log.debug("CustomErrorController invoked");
+		
 		throw new InternalServerErrorException("Unknown error occured");
 	}
 
