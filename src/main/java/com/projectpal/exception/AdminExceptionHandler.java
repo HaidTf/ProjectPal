c4d28e69a9c85fed64385a1854f645f0f4dec07d
haidtf <haidartfeily25@gmail.com>
2023-11-10 21:19:57 +0200

    refactor(AdminExceptionHandler):add logging functionality using Slf4j

--- src/main/java/com/projectpal/exception/AdminExceptionHandler.java
+++ src/main/java/com/projectpal/exception/AdminExceptionHandler.java
@@ -7,11 +7,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.projectpal.dto.response.exception.ExceptionResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestControllerAdvice("com.projectpal.controller.admin")
+@Slf4j
 public class AdminExceptionHandler {
 
 	@ExceptionHandler(AccessDeniedException.class)
 	public ResponseEntity<ExceptionResponse> handleAccessDeniedException(AccessDeniedException ex) {
+		
+		log.info("Unauthorized access attempt to ADMIN endpoints");
+		
 		return ResponseEntity.status(404).body(new ExceptionResponse("Endpoint not found"));
 	}
 
