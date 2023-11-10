c8bcc358bd6464925ef4eb2c7cee91f143f4b81d
haidtf <haidartfeily25@gmail.com>
2023-11-10 21:22:12 +0200

    refactor:add logging functionality using Slf4j to authentication controllers

--- src/main/java/com/projectpal/controller/authentication/LoginController.java
+++ src/main/java/com/projectpal/controller/authentication/LoginController.java
@@ -16,26 +16,34 @@ import com.projectpal.service.authentication.AuthenticationService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/auth/login")
 @RequiredArgsConstructor
+@Slf4j
 public class LoginController {
 
 	private final AuthenticationService authService;
 
 	@ExceptionHandler(AuthenticationException.class)
 	public ResponseEntity<ExceptionResponse> handleException(AuthenticationException ex) {
+
 		if (ex instanceof BadCredentialsException) {
+
+			log.info("API:Authentication failed. Incorrect email or password ");
+
 			return ResponseEntity.status(401).header("WWW-Authenticate", "Basic realm=\"User Login\"")
 					.body(new ExceptionResponse("Login failed. Incorrect email or password"));
 		} else {
-			return ResponseEntity.status(500).body(new ExceptionResponse("Login failed. Unknown error occured"));
+			throw ex;
 		}
 	}
 
 	@PostMapping("")
 	public ResponseEntity<AuthenticationResponse> authenticate(@Valid @RequestBody AuthenticationRequest request) {
+
+		log.info("API:POST/api/auth/login invoked with email:{}", request.getEmail());
 
 		return ResponseEntity.ok(authService.authenticate(request));
 	}
--- src/main/java/com/projectpal/controller/authentication/RegisterController.java
+++ src/main/java/com/projectpal/controller/authentication/RegisterController.java
@@ -11,29 +11,38 @@ import org.springframework.web.bind.annotation.RestController;
 import com.projectpal.dto.request.authentication.RegisterRequest;
 import com.projectpal.dto.response.AuthenticationResponse;
 import com.projectpal.dto.response.exception.DataIntegrityExceptionResponse;
-import com.projectpal.exception.server.InternalServerErrorException;
 import com.projectpal.service.authentication.AuthenticationService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/auth/register")
 @RequiredArgsConstructor
+@Slf4j
 public class RegisterController {
 
 	private final AuthenticationService authService;
 
 	@ExceptionHandler(DataIntegrityViolationException.class)
 	public ResponseEntity<DataIntegrityExceptionResponse> handleException(DataIntegrityViolationException ex) {
+		
 		DataIntegrityExceptionResponse response = new DataIntegrityExceptionResponse(ex);
-		if(response.isConstraintViolation())
+		
+		if (response.isConstraintViolation()) {
+			
+			log.info("API:Registration failed due to constraint violation: {}", response.getConstraintViolated());
+
 			return ResponseEntity.status(409).body(response);
-		throw new InternalServerErrorException("unknown error occured during registration");
+		}
+		throw ex;
 	}
 
 	@PostMapping("")
 	public ResponseEntity<AuthenticationResponse> register(@Valid @RequestBody RegisterRequest request) {
+
+		log.info("API:POST/api/auth/register invoked with email:{}", request.getEmail());
 
 		AuthenticationResponse response = authService.register(request);
 
