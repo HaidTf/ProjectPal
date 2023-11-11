642462f65cb529dbbc92e6c7b199b62904424e87
haidtf <haidartfeily25@gmail.com>
2023-11-11 02:34:10 +0200

    refactor(CustomAuthenticationEntryPoint): add logging functionality using Slf4j

--- src/main/java/com/projectpal/security/entrypoint/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/projectpal/security/entrypoint/CustomAuthenticationEntryPoint.java
@@ -9,21 +9,25 @@ import org.springframework.stereotype.Component;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
 
+		log.debug("CustomAuthenticationEntryPoint invoked due to exception: {}", authException.getMessage());
+
 		response.setStatus(401);
 
 		response.addHeader("WWW-Authenticate", "Bearer");
-		
+
 		response.setContentType("application/json;charset=UTF-8");
 
-		String jsonResponse = new StringBuilder().append("{\"message\":").append("\"").append(authException.getMessage())
-				.append("\"}").toString();
+		String jsonResponse = new StringBuilder().append("{\"message\":").append("\"")
+				.append(authException.getMessage()).append("\"}").toString();
 
 		response.getWriter().write(jsonResponse);
 	}
